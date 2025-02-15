3c05315fd63e47398fdb44feddfcc837dd1dd15d
Charvi Wadhwa <154724710+charrviwadhwa@users.noreply.github.com>
2025-02-15 23:53:28 +0530

    Update

---
+++ 1303A-Erasing zeroes.java
@@ -0,0 +1,40 @@
+
+
+import java.util.Scanner;
+
+public class Problem_1303A {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 Scanner sc = new Scanner(System.in);
+	        int t = sc.nextInt();  
+	      
+
+	        while (t-- > 0) {
+	            String s = sc.next();
+	            int firstOne = -1, lastOne = -1, zero = 0;
+
+	            for (int i = 0; i < s.length(); i++) {
+	                if (s.charAt(i) == '1') {
+	                    if (firstOne == -1) firstOne = i;
+	                    lastOne = i;
+	                }
+	            }
+
+	           
+	            if (firstOne == -1) {
+	            	System.out.println("0");
+	            	continue;
+	            }
+
+	            
+	            for (int i = firstOne; i <= lastOne; i++) {
+	                if (s.charAt(i) == '0') zero++;
+	            }
+
+	            System.out.println(zero);
+	        }
+
+	}
+
+}
