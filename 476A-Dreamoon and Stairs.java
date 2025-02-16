883b1f46be8bef6d4cb58d3a07ab694266872e9e
Charvi Wadhwa <154724710+charrviwadhwa@users.noreply.github.com>
2025-02-17 04:24:19 +0530

    Create 476A-Dreamoon and Stairs.java

---
+++ 476A-Dreamoon and Stairs.java
@@ -0,0 +1,23 @@
+package CodeForces;
+
+import java.util.Scanner;
+
+public class Problem_476A {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int m=sc.nextInt();
+		int min=(n+1)/2;
+		int ans=(min+m-1)/m*m;
+		if(ans<=n) {
+			System.out.println(ans);
+		}
+		else {
+			System.out.println(-1);
+		}
+
+	}
+
+}
